40be35ed8e561202fef508c8768ac9dd18625e8f
BekzhanJoy <bekzhan_30.09.2004@mail.ru>
2025-02-14 15:38:11 +0500

    Я крутой парень сделал так чтобы кнопки не было видно в login and register page

--- app/src/main/java/com/example/fittrack/MainActivity.kt
+++ app/src/main/java/com/example/fittrack/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.fittrack
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -14,9 +15,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.loginFragment || destination.id == R.id.registerFragment) {
+                binding.bottomNavigationView.visibility = View.GONE
+            } else {
+                binding.bottomNavigationView.visibility = View.VISIBLE
+            }
+        }
+
     }
 }
